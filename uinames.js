a797df92c3b0dbea3b19909acc4750bf62d7e706
cbestiva <cbestiva@gmail.com>
2017-05-10 12:19:47 -0700

    getFemaleUserPromise returns a female user object and passes test

--- uinames.js
+++ uinames.js
@@ -6,23 +6,45 @@ getFakeUser = function() {
     "surname":"Doe",
     "gender":"male",
     "region":"United States"
-  };
+  }
   return user
 }
 
 getTwoApiFakeUsers = function(callback) {
   var options = {
-    url: 'http://uinames.com/api/?amount=2&region=united%20states'
+    url: 'http://uinames.com/api/?amount=2'
   }
 
   request(options, function(err, res, body) {
     apiUsers = JSON.parse(body)
     console.log('user: ', apiUsers)
     return callback(apiUsers)
-  });
+  })
+}
+
+getFemaleFakeUserPromise = function() {
+  var options ={
+    url: 'http://uinames.com/api/?gender=female',
+    json: true
+  }
+
+  return apiPromise = new Promise((resolve, reject) => {
+    request(options, function(err, res, body) {
+      if(err) {
+        reject(err)
+      } else {
+        resolve(body)
+      }
+    })
+    // femaleApiUser is whatever was passed in resolve()
+  }).then((femaleApiUser) => {
+    console.log('female api user = ', femaleApiUser)
+    return femaleApiUser;
+  })
 }
 
 module.exports = {
   getFakeUser,
-  getTwoApiFakeUsers
+  getTwoApiFakeUsers,
+  getFemaleFakeUserPromise
 }
