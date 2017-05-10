5156a06df48f2a4a6b99eaf58be131477d9d5967
cbestiva <cbestiva@gmail.com>
2017-05-09 20:56:59 -0700

    getFakeUser returns a user object and passes test

--- uinames.js
+++ uinames.js
@@ -1,3 +1,13 @@
-getFakeUser = function() {}
+var request = require('request')
+
+getFakeUser = function() {
+  let user = {
+    "name":"John",
+    "surname":"Doe",
+    "gender":"male",
+    "region":"United States"
+  };
+  return user;
+}
 
 module.exports = getFakeUser;
