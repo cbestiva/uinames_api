102755954a760f385035fb33fcd33eb0a7f1ddad
cbestiva <cbestiva@gmail.com>
2017-05-09 22:44:56 -0700

    getTwoApiFakeUsers returns array of two user objects from api request and passes test

--- uinames.js
+++ uinames.js
@@ -10,4 +10,19 @@ getFakeUser = function() {
   return user
 }
 
-module.exports = getFakeUser
+getTwoApiFakeUsers = function(callback) {
+  var options = {
+    url: 'http://uinames.com/api/?amount=2&region=united%20states'
+  }
+
+  request(options, function(err, res, body) {
+    apiUsers = JSON.parse(body)
+    console.log('user: ', apiUsers)
+    return callback(apiUsers)
+  });
+}
+
+module.exports = {
+  getFakeUser,
+  getTwoApiFakeUsers
+}
